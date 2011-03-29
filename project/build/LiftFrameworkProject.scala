f51c6a7a352770369c77eadb547e04a8672f6096
Indrajit Raychaudhuri <irc@indrajit.com>
2011-03-29 21:11:48 +0530

    scalajpa temporary update for 2.9.0.RC1

--- project/build/LiftFrameworkProject.scala
+++ project/build/LiftFrameworkProject.scala
@@ -50,6 +50,7 @@ class LiftFrameworkProject(info: ProjectInfo) extends ParentProject(info) with L
   lazy val proto          = persistenceProject("proto")(webkit)
 // FIXME: Scala 2.9.0.RC1
 //  lazy val jpa            = persistenceProject("jpa", scalajpa, persistence_api)(webkit)
+  lazy val jpa            = persistenceProject("jpa", "org.scala-libs" %% "scalajpa" % "1.4-SNAPSHOT", persistence_api)(webkit)
   lazy val mapper         = persistenceProject("mapper", RuntimeScope.h2database, RuntimeScope.derby)(db, proto)
   lazy val record         = persistenceProject("record")(mapper)
   lazy val ldap           = persistenceProject("ldap", TestScope.apacheds)(mapper)
