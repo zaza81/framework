aa110cb9d5f81d1987a66fc1753e3b4ed4dcb6d5
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-06-21 17:57:02 -0400

    Make build-api-docs script work standalone.
    Running docs/scripts/build-api-docs.sh should now properly
    send the resulting API documentation to target/docs/api.

--- docs/scripts/build-api-docs.sh
+++ docs/scripts/build-api-docs.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
 WORK_DIR=`pwd`
 
 if [[ ! -z "$DEXY_ROOT" ]]
 then
   cd $DEXY_ROOT
+else
+  # if we're not in dexy, work in target/docs, which can be regenerated anyway
+  WORK_DIR=target/docs
 fi
 
 sbt -Dsbt.log.noformat=true doc
