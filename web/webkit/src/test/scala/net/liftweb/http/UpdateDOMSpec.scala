cbc4fb0b09f46fc810fe7eedd396deb7dda2e019
Joe Barnes <barnesjd@gmail.com>
2016-04-17 20:22:57 -0500

    Added a second VDom diff and patch test, refactoring to make it clean

--- web/webkit/src/test/scala/net/liftweb/http/UpdateDOMSpec.scala
+++ web/webkit/src/test/scala/net/liftweb/http/UpdateDOMSpec.scala
@@ -21,21 +21,74 @@ object UpdateDOMSpec extends Specification with XmlMatchers {
 
   "UpdateDOM Spec".title
 
-  "UpdateDOM" should {
-    "add a <li>" in {
-      val lift_js = this.getClass.getClassLoader.getResource("toserve/lift.js")
-      val jq_js   = this.getClass.getClassLoader.getResource("toserve/jquery-1.4.4.js")
-      def html(body:Node) =
-        <html>
-          <head>
-            <meta charset="UTF-8"/>
-            <title>Home</title>
-            <script type="application/javascript" language="javascript" src={jq_js.toURI.toASCIIString}></script>
-            <script type="application/javascript" language="javascript" src={lift_js.toURI.toASCIIString}></script>
-          </head>
-          {body}
-        </html>
+  def updateAndCompare(before:Node, after:Node) = {
+    val lift_js = this.getClass.getClassLoader.getResource("toserve/lift.js")
+    val jq_js   = this.getClass.getClassLoader.getResource("toserve/jquery-1.4.4.js")
+    def html(body:Node) =
+      <html>
+        <head>
+          <meta charset="UTF-8"/>
+          <title>Home</title>
+          <script type="application/javascript" language="javascript" src={jq_js.toURI.toASCIIString}></script>
+          <script type="application/javascript" language="javascript" src={lift_js.toURI.toASCIIString}></script>
+        </head>
+        {body}
+      </html>
 
+    val diff = VDom.diff(before, after)
+    val js = JE.Call("lift.updateBody", Extraction.decompose(diff)).toJsCmd
+
+    val file = File.createTempFile("test", "html")
+    try {
+      val w = new FileWriter(file)
+      w.write("<!DOCTYPE html>")
+      Html5.write(html(before), w, true, true)
+      w.close()
+      val client = new WebClient(CHROME)
+      val options = client.getOptions()
+      options.setHomePage(WebClient.URL_ABOUT_BLANK.toString())
+      options.setJavaScriptEnabled(true)
+
+      client.getPage(file.toURI.toURL)
+      val window = client.getCurrentWindow().getTopWindow
+      val page:HtmlPage = window.getEnclosedPage().asInstanceOf[HtmlPage] // asInstanceOf because ... java...
+
+      def toXml(e:DomNode):Node = {
+        val attrs:MetaData = (0 until e.getAttributes.getLength).foldLeft(Null:MetaData) {
+          case (acc, i) =>
+            val attr = e.getAttributes.item(i)
+            new UnprefixedAttribute(attr.getNodeName, Text(attr.getNodeValue), acc)
+        }
+        val children = e.getChildren.asScala.map(toXml).toSeq
+        if(e.getNodeName == "#text") Text(e.getTextContent)
+        else Elem(null, e.getNodeName, attrs, TopScope, true, children:_*)
+      }
+
+      def exec(js:String):String = {
+        val toRun = "function() {\n"+js+"\n};"
+        val result = page.executeJavaScript(toRun)
+        val func:JsFunction = result.getJavaScriptResult().asInstanceOf[JsFunction]
+
+        val exeResult = page.executeJavaScriptFunctionIfPossible(
+          func,
+          window.getScriptableObject(),
+          Array.empty,
+          page.getDocumentElement()
+        )
+
+        exeResult.getJavaScriptResult.toString
+      }
+
+      exec(js)
+
+      toXml(page.getBody) must beEqualToIgnoringSpace(after)
+    } finally {
+      file.delete()
+    }
+  }
+
+  "UpdateDOM" should {
+    "append a <li>" in {
       val before =
         <body data-lift-content-id="main">
           <div>
@@ -59,56 +112,35 @@ object UpdateDOMSpec extends Specification with XmlMatchers {
           </div>
         </body>
 
-      val diff = VDom.diff(before, after)
-      val js = JE.Call("lift.updateBody", Extraction.decompose(diff)).toJsCmd
+      updateAndCompare(before, after)
+    }
 
-      val file = File.createTempFile("test", "html")
-      try {
-        val w = new FileWriter(file)
-        w.write("<!DOCTYPE html>")
-        Html5.write(html(before), w, true, true)
-        w.close()
-        val client = new WebClient(CHROME)
-        val options = client.getOptions()
-        options.setHomePage(WebClient.URL_ABOUT_BLANK.toString())
-        options.setJavaScriptEnabled(true)
+    "append two <li>'s" in {
+      val before =
+        <body data-lift-content-id="main">
+          <div>
+            <hr/>
+            <ul>
+              <li>Message 1</li>
+              <li>Message 2</li>
+            </ul>
+          </div>
+        </body>
 
-        client.getPage(file.toURI.toURL)
-        val window = client.getCurrentWindow().getTopWindow
-        val page:HtmlPage = window.getEnclosedPage().asInstanceOf[HtmlPage] // asInstanceOf because ... java...
+      val after =
+        <body data-lift-content-id="main">
+          <div>
+            <hr/>
+            <ul>
+              <li>Message 1</li>
+              <li>Message 2</li>
+              <li>Message 3</li>
+              <li>Message 4</li>
+            </ul>
+          </div>
+        </body>
 
-        def toXml(e:DomNode):Node = {
-          val attrs:MetaData = (0 until e.getAttributes.getLength).foldLeft(Null:MetaData) {
-            case (acc, i) =>
-              val attr = e.getAttributes.item(i)
-              new UnprefixedAttribute(attr.getNodeName, Text(attr.getNodeValue), acc)
-          }
-          val children = e.getChildren.asScala.map(toXml).toSeq
-          if(e.getNodeName == "#text") Text(e.getTextContent)
-          else Elem(null, e.getNodeName, attrs, TopScope, true, children:_*)
-        }
-
-        def exec(js:String):String = {
-          val toRun = "function() {\n"+js+"\n};"
-          val result = page.executeJavaScript(toRun)
-          val func:JsFunction = result.getJavaScriptResult().asInstanceOf[JsFunction]
-
-          val exeResult = page.executeJavaScriptFunctionIfPossible(
-            func,
-            window.getScriptableObject(),
-            Array.empty,
-            page.getDocumentElement()
-          )
-
-          exeResult.getJavaScriptResult.toString
-        }
-
-        exec(js)
-
-        toXml(page.getBody) must beEqualToIgnoringSpace(after)
-      } finally {
-        file.delete()
-      }
+      updateAndCompare(before, after)
     }
   }
 
